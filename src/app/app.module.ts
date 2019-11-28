29cd024c236576522f259e74ccd4f72dc68c78bd
Mithilesh <mithileshaws123@gmail.com>
2019-11-28 16:19:51 +0530

    adding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { DeleteComponent } from './delete/delete.component';
+import { RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { EditComponent } from './edit/edit.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthService } from './auth.service';
 
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    DeleteComponent,
+    EditComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+      RouterModule.forRoot([
+       {path:"",component:HomeComponent,canActivate:[AuthService]},   
+        {path:"home",component:HomeComponent,canActivate:[AuthService]},
+        {path:"delete/:id",component:DeleteComponent,canActivate:[AuthService]},
+        {path:"register",component:RegisterComponent,canActivate:[AuthService]},
+        {path:"edit/:id",component:EditComponent,canActivate:[AuthService]},
+        {path:"login",component:LoginComponent}
+      
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
